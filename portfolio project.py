b5ed315ce0753bd4652c61349433d2294c0bf2b3
afiefah-s <afiefah_sahebdin@hotmail.com>
2026-05-16 13:26:10 +0200

    Update portfolio project.py

--- portfolio project.py
+++ portfolio project.py
@@ -7,7 +7,7 @@ import plotly.graph_objects as go
 import streamlit as st
 
 st.set_page_config(
-    page_title="HSP Compatibility",
+    page_title="HSP Beauty Match",
     page_icon="🧴",
     layout="wide",
 )
